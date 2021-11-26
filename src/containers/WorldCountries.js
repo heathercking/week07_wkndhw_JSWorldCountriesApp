485590978ea8fcdf15cd88c2e12e470476f24eba
Heather King <hkingdevelopment@gmail.com>
2021-11-26 16:36:33 +0000

    adds set up of App and WorldCountries container

---
+++ src/containers/WorldCountries.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const WorldCountries = () => {
+
+    return (
+        <h1>I am the WorldCountries container</h1>
+    )
+
+}
+
+export default WorldCountries;
